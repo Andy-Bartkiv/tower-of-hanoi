0a31d93b24f3e7e4720340716a7bbf4dc993f9fa
Andy-W <andy.bartkiv@gmail.com>
2021-09-25 20:25:33 +0300

    add styles.css to build

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 console.log('Salut - 2! Tower of Hanoi is running.');
 console.log('Update attempt #1')
+
+import './styles.css';
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = 'Hello Webpack';
+    element.classList.add('hello');
+
+    return element;
+ }
+
+ document.body.appendChild(component());
